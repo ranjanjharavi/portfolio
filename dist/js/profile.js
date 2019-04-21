491715fd3aafacf33fe9a33988652bf6c7833975
ranjanjharavi <ranjanjharavi@gmail.com>
2019-04-21 12:49:44 +0530

    added few animations and project section made scss files somewhat modular

---
+++ dist/js/profile.js
@@ -0,0 +1,20 @@
+let index = 0;
+let deleting = false;
+
+const abc = () => {
+  const type = document.getElementById("effect");
+  const text = type.getAttribute("data-word");
+  let displayText = "";
+
+  displayText = text.substring(0, index);
+  type.innerHTML = '<span class="abc">' + displayText + "</span>";
+
+  if (deleting) index -= 1;
+  else index += 1;
+
+  if (index === text.length + 1) deleting = true;
+  else if (index === 0) deleting = false;
+
+  setTimeout(abc, 300);
+};
+abc();
